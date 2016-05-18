0321d1506a9b4bf8704fe5e86e5ace2fa5248b82
Praveen C <cpraveen@gmail.com>
2016-05-18 09:51:59 +0530

    Add missing include file

--- src/bvte.cc
+++ src/bvte.cc
@@ -46,6 +46,7 @@
 #include <deal.II/dofs/dof_tools.h>
 #include <deal.II/fe/fe_dgq.h>
 #include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/mapping_q.h>
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/numerics/vector_tools.h>
 
